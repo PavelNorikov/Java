aa2a53c2fdd2633cad85ed13c3af3ea86c5d7595
PavelNorikov <100146105+PavelNorikov@users.noreply.github.com>
2024-05-24 16:18:00 +0400

    Create Main.java

---
+++ src/main/java/ru/sgu/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Hello t1 = new Hello();
+        //t1.sayHello();
+
+        Operations t2 = new Operations();
+        t2.getSolve();
+
+        File_Scanner t3 = new File_Scanner();
+        //t3.start();
+    }
+}
