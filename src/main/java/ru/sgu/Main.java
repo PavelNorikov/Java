58cf99a0435ba13a6d49ea670f43e66268e6af1e
PavelNorikov <100146105+PavelNorikov@users.noreply.github.com>
2024-05-24 16:25:34 +0400

    Create Main.java

---
+++ src/main/java/ru/sgu/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        t1 t1 = new t1();
+        t1.run();
+
+        t2 t2 = new t2();
+        t2.run();
+
+        t3 t3 = new t3();
+        t3.run();
+    }
+}
