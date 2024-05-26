3f7fc25a34b9105eea9601d3b075a6de86a84696
PavelNorikov <100146105+PavelNorikov@users.noreply.github.com>
2024-05-27 02:17:10 +0400

    Update Main.java

--- src/main/java/ru/sgu/Main.java
+++ src/main/java/ru/sgu/Main.java
@@ -3,12 +3,12 @@
 public class Main {
     public static void main(String[] args) {
         Hello t1 = new Hello();
-        //t1.sayHello();
+        t1.sayHello();
 
         Operations t2 = new Operations();
         t2.getSolve();
 
         File_Scanner t3 = new File_Scanner();
-        //t3.start();
+        t3.start();
     }
 }
